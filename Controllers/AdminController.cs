5bc297dd79ee9e0e314d115e2b0afba30e5481a8
msenevirathne <114546688+msenevirathne@users.noreply.github.com>
2024-09-08 21:46:01 +0530

    create role

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,62 @@
+﻿using AspNetCoreIdentityDemo.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIdentityDemo.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                bool roleExists = await _roleManager.RoleExistsAsync(model.RoleName);
+
+                if(roleExists) 
+                {
+                    ModelState.AddModelError("", "Role already exists.");
+                }
+                else
+                {
+                    var identityRole = new IdentityRole 
+                    { 
+                        Name = model.RoleName 
+                    };
+
+                    var result = await _roleManager.CreateAsync(identityRole);
+
+                    if(result.Succeeded) 
+                    {
+                        return RedirectToAction(nameof(HomeController.Index), "Home");
+                    }
+
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+
+            return View(model);
+        }
+
+    }
+}
